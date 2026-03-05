7f392026b5422c615442c564099ae80ed2b7d2c9
parthkorat700-crypto <parthkorat700@gmail.com>
2026-03-05 23:50:09 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,46 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero"
+    return a / b
+
+
+while True:
+    print("\nSimple Calculator")
+    print("1. Add (+)")
+    print("2. Subtract (-)")
+    print("3. Multiply (*)")
+    print("4. Divide (/)")
+    print("5. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == "5":
+        print("Exiting calculator...")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == "1":
+        print("Result:", add(num1, num2))
+
+    elif choice == "2":
+        print("Result:", subtract(num1, num2))
+
+    elif choice == "3":
+        print("Result:", multiply(num1, num2))
+
+    elif choice == "4":
+        print("Result:", divide(num1, num2))
+
+    else:
+        print("Invalid choice. Try again.")
